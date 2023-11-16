b4656fd1bae12b2a0073d07a15738cbbfccae3c5
Adnan <adnanammidouah9@gmail.com>
2023-11-16 21:48:05 +0100

    Makefile

---
+++ code/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("Adnane Ammidouah \n");
+
+
+
+
+
+    return 0;
+}
---
+++ lib/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "test.h"
+#include "led.h"
+int main()
+{
+    Display();
+    ledon();
+    ledoff();
+    return 0;
+}
